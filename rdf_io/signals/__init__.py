9c9d8cac677dc7f823421168a3193482932e1dba
Thomas Fillon <thomas@parisson.com>
2017-02-25 16:12:26 +0100

    Fix issue: models can not be imported at init time

--- rdf_io/signals/__init__.py
+++ rdf_io/signals/__init__.py
@@ -1,4 +1,5 @@
-from rdf_io.signals.utils import *
+#from rdf_io.signals.utils import sync_signals
  
-signals.post_save.connect(setup_signals, sender=ObjectMapping)
-sync_signals()
+#signals.post_save.connect(setup_signals, sender=ObjectMapping)
+#sync_signals()
+#  TODO: signals need to be fixed something must be broken now
